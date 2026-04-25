92c4bcbad4ca0b43e4f0859389d533e22324ff6f
Ahmed Talal <ahmed.talal.egy@gmail.com>
2026-04-25 18:06:05 +0300

    Finish setting up auth and dashboard structures

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='flex item-center m-auto'>Login-page</div>
+    )
+}
---
+++ src/app/(dashboard)/(diplomas)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+
+
+export default function Home() {
+  return (
+    <div className="font-">Home page
+
+    </div >
+  );
+}
